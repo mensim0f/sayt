b236ea2b9530e4faa07cc57d1571db80d1f50dbb
mensim0f <64090070+mensim0f@users.noreply.github.com>
2020-11-28 11:23:51 +0400

    Update giris.php

--- phpsite/giris.php
+++ phpsite/giris.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("sql309.phpnet.us","pn_25338447","190405314r","pn_25338447_user");
+$db = mysqli_connect("","","","");
 
 if(mysqli_connect_errno()){
 	echo "Baza ile elaqe alinmadi";
